2e10f1214efabbac1ecbe9933f70041094ceec35
BIT <BIT@DESKTOP-08BCIT0>
2019-08-05 20:40:43 +0900

    testtest

--- src/testgithub.java
+++ src/testgithub.java
@@ -4,6 +4,7 @@ public class testgithub {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.printf("Test Github");
+		System.out.printf("Test Github");
 	}
 
 }
